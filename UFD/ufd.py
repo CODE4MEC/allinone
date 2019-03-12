5fd7514dc65b9991d6778a99acf2d914a7c6db0e
Devin Yang <4devinyang@gmail.com>
2019-03-12 19:53:12 +0800

    no message

--- UFD/ufd.py
+++ UFD/ufd.py
@@ -24,10 +24,12 @@ from ryu.app.wsgi import Response
 from ryu.app.wsgi import route
 from ryu.app.wsgi import WSGIApplication
 
-ufd_app_name = 'ufd_app'
-url_overload = '/ufd/port'
-url_port_statistic = '/ufd/port-statistic'
-window = 10
+ufd_app_name = 'ufd_app' 
+url_overload = '/ufd/port' 
+url_port_statistic = '/ufd/port-statistic' 
+window = 10 
+alpha = 0.2 
+step = 0.1 
 
 filename = '/home/node2/bots-remain'
 
@@ -37,12 +39,15 @@ class SimpleMonitor13(app_manager.RyuApp):
     _CONTEXTS = {'wsgi': WSGIApplication}
     # _CONTEXTS = {'dpset': dpset.DPSet}
 
+
+
     def __init__(self, *args, **kwargs):
         super(SimpleMonitor13, self).__init__(*args, **kwargs)
         self.port_stats = {}
         self.flow_stats = {}
         self.delete_ports = {}
         self.datapaths = {}
+        self.new_port = None
         self.monitor_thread = hub.spawn(self._monitor)
 
         wsgi = kwargs['wsgi']
@@ -60,29 +65,41 @@ class SimpleMonitor13(app_manager.RyuApp):
                     body = body + '%d '%(port) + '%d\n'%(self.port_stats[dp_id][port]['stats'])
         return body
 
-    def clean_overload_port(self, port):
+    def clean_overload_port(self, port): 
 #        self.logger.info('%d', port)
+        user_list = ''
         for dp_id in self.datapaths:
-            if (port in self.port_stats[dp_id]) and (port != 10001):
+            if (port in self.port_stats[dp_id]) and (port != 10001): 
 
                 connections = []
                 for connection in self.port_stats[dp_id][port]['users']:
                     if self.flow_stats[dp_id].has_key(connection):
                         connections.append((self.flow_stats[dp_id][connection][(connection[0], connection[1])]+self.flow_stats[dp_id][connection][(connection[1], connection[0])], connection))
                 connections.sort(reverse = True)
+                R = 0.5 * sum([pair[0] for pair in connections])
 
 
-                for i in range(0, len(connections), 2):
-                    connection = connections[i][1]
-                    self.port_stats[dp_id][port]['users'].discard(connection)
-                    if self.flow_stats[dp_id].has_key(connection):
-                        del self.flow_stats[dp_id][connection]
-                    dp = self.datapaths[dp_id]
-                    parser = dp.ofproto_parser
-                    match_s2d = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=connection[0], ipv4_src=connection[1])
-                    match_d2s = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=connection[1], ipv4_src=connection[0])
-                    self.del_flow(dp, match_s2d, port)
-                    self.del_flow(dp, match_d2s, port)
+                for i in range(0, len(connections), 1):
+                    if R <= 0:
+                        break
+                    elif R >= connections[i][0]:
+                        R -= connections[i][0]
+                        connection = connections[i][1]
+                        user_list = user_list + connection[0] +' '+connection[1]+'\n'
+                        self.port_stats[dp_id][port]['users'].discard(connection)
+                        if self.flow_stats[dp_id].has_key(connection):
+                            del self.flow_stats[dp_id][connection]
+                        dp = self.datapaths[dp_id]
+                        parser = dp.ofproto_parser
+                        match_s2d = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=connection[0], ipv4_src=connection[1])
+                        match_d2s = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=connection[1], ipv4_src=connection[0])
+                        if self.new_port and self.new_port not in self.port_stats[dp_id]:
+                            actions = [parser.OFPActionSetField(eth_dst="66:66:66:66:66:66"), parser.OFPActionOutput(self.new_port)]
+                            self.add_flow(dp, 1, match_d2s, actions)
+                            self.add_flow(dp, 1, match_s2d, actions)
+                        self.del_flow(dp, match_s2d, port)
+                        self.del_flow(dp, match_d2s, port)
+        return user_list
 
 #                file = open(filename, 'w')
 
@@ -120,19 +137,19 @@ class SimpleMonitor13(app_manager.RyuApp):
 
 
 
-
+    
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         match = parser.OFPMatch()
-        self.del_flow(datapath, match)
-        match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_dst='192.168.95.0/24')
+        self.del_flow(datapath, match) 
+        match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_dst='192.168.95.0/24') 
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
 #        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
-        match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_ARP)
+        match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_ARP) 
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
 #        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
@@ -154,7 +171,7 @@ class SimpleMonitor13(app_manager.RyuApp):
             self.delete_ports.setdefault(datapath.id, set())
             self.flow_stats[datapath.id] = {}
 
-    @set_ev_cls(ofp_event.EventOFPPortDescStatsReply, MAIN_DISPATCHER)
+    @set_ev_cls(ofp_event.EventOFPPortDescStatsReply, MAIN_DISPATCHER) 
     def port_desc_stats_reply_handler(self, ev):
         msg = ev.msg
         dp = msg.datapath
@@ -162,22 +179,22 @@ class SimpleMonitor13(app_manager.RyuApp):
         parser = dp.ofproto_parser
         for port in ev.msg.body:
             if port.port_no < 5000:
-                match = parser.OFPMatch(in_port=port.port_no, eth_type=ether_types.ETH_TYPE_IP)
+                match = parser.OFPMatch(in_port=port.port_no, eth_type=ether_types.ETH_TYPE_IP) 
                 actions = [parser.OFPActionOutput(10001)]
                 self.add_flow(dp, 1, match, actions)
 
     @set_ev_cls(ofp_event.EventOFPStateChange,
-                [MAIN_DISPATCHER, DEAD_DISPATCHER])
+                [MAIN_DISPATCHER, DEAD_DISPATCHER]) 
     def _state_change_handler(self, ev):
         datapath = ev.datapath
         if ev.state == MAIN_DISPATCHER:
-            if datapath.id not in self.datapaths:
+            if datapath.id not in self.datapaths: 
                 self.logger.debug('register datapath: %016x', datapath.id)
                 self.datapaths[datapath.id] = datapath
                 self.flow_stats[datapath.id] = {}
                 self.port_stats.setdefault(datapath.id, {})
                 self.delete_ports.setdefault(datapath.id, set())
-        elif ev.state == DEAD_DISPATCHER:
+        elif ev.state == DEAD_DISPATCHER: 
             if datapath.id in self.datapaths:
                 self.logger.debug('unregister datapath: %016x', datapath.id)
                 if self.datapaths.has_key(datapath.id):
@@ -189,7 +206,7 @@ class SimpleMonitor13(app_manager.RyuApp):
                 if self.flow_stats.has_key(datapath.id):
                     del self.flow_stats[datapath.id]
 
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None): 
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
@@ -217,7 +234,7 @@ class SimpleMonitor13(app_manager.RyuApp):
                                 match=match)
         datapath.send_msg(mod)
 
-    @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
+    @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER) 
     def port_status_handler(self, ev):
         msg = ev.msg
         dp = msg.datapath
@@ -226,18 +243,19 @@ class SimpleMonitor13(app_manager.RyuApp):
         port_no = msg.desc.port_no
         match = parser.OFPMatch(in_port=port_no, eth_type=ether_types.ETH_TYPE_IP)
         if port_no < 10000:
-            if msg.reason == ofp.OFPPR_ADD:
+            if msg.reason == ofp.OFPPR_ADD: 
                 if port_no not in self.port_stats[dp.id]:
                     # self.port_stats[dp.id].setdefault(port_no, {'stats':[0 for i in range(window)], 'users':set()})
                     self.port_stats[dp.id].setdefault(port_no, {'stats':0, 'users':set()})
-                if port_no < 5000:
+                    self.new_port = port_no
+                if port_no < 5000: 
                     actions = [parser.OFPActionOutput(10001)]
                     self.add_flow(dp, 1, match, actions)
             elif msg.reason == ofp.OFPPR_DELETE:
-                if port_no < 5000:
+                if port_no < 5000: 
                     self.del_flow(dp, match)
                 match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP)
-                self.del_flow(dp, match, port_no)
+                self.del_flow(dp, match, port_no) 
                 if port_no in self.port_stats[dp.id]:
                     for connection in self.port_stats[dp.id][port_no]['users']:
                         if self.flow_stats[dp.id].has_key(connection):
@@ -250,26 +268,26 @@ class SimpleMonitor13(app_manager.RyuApp):
         self.logger.info('OFPPortStatus received: port_no=%d', port_no)
         print msg.reason
 
-    def _monitor(self):
+    def _monitor(self): 
         while True:
             for dp in self.datapaths.values():
                 self._request_stats(dp)
             hub.sleep(0.5)
 
-    def _request_stats(self, datapath):
+    def _request_stats(self, datapath): 
         self.logger.debug('send stats request: %016x', datapath.id)
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
         match = parser.OFPMatch(in_port=10001, eth_type=ether_types.ETH_TYPE_IP)
         cookie = cookie_mask = 0
-        req = parser.OFPFlowStatsRequest(datapath, 0, ofproto.OFPTT_ALL, ofproto.OFPP_ANY, ofproto.OFPG_ANY, cookie, cookie_mask, match)
+        req = parser.OFPFlowStatsRequest(datapath, 0, ofproto.OFPTT_ALL, ofproto.OFPP_ANY, ofproto.OFPG_ANY, cookie, cookie_mask, match) 
         datapath.send_msg(req)
 
-        req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
+        req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY) 
         datapath.send_msg(req)
 
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER) 
     def _packet_in_handler(self, ev):
         if ev.msg.msg_len < ev.msg.total_len:
             self.logger.debug("packet truncated: only %s of %s bytes",
@@ -290,9 +308,9 @@ class SimpleMonitor13(app_manager.RyuApp):
             src_ip = arp_pkt.src_ip
             dst_ip = arp_pkt.dst_ip
 
-            if (opcode == arp.ARP_REQUEST) and (in_port < 5000) and (src_ip == '192.168.97.101') and (dst_ip == '192.168.97.1'):
+            if (opcode == arp.ARP_REQUEST) and (in_port < 5000) and (src_ip == '192.168.97.101') and (dst_ip == '192.168.97.1'): 
                 actions = [parser.OFPActionOutput(10001)]
-            elif (opcode == arp.ARP_REPLY) and (in_port == 10001) and (src_ip == '192.168.97.1') and (dst_ip == '192.168.97.101'):
+            elif (opcode == arp.ARP_REPLY) and (in_port == 10001) and (src_ip == '192.168.97.1') and (dst_ip == '192.168.97.101'): 
                 actions = []
                 for port_no in self.port_stats[dpid].keys():
                     if port_no < 5000:
@@ -304,33 +322,38 @@ class SimpleMonitor13(app_manager.RyuApp):
             if msg.buffer_id == ofproto.OFP_NO_BUFFER:
                 data = msg.data
 
-            out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=data)
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=data) 
             datapath.send_msg(out)
 
-        elif (eth.ethertype == ether_types.ETH_TYPE_IP) and (in_port == 10001):
+        elif (eth.ethertype == ether_types.ETH_TYPE_IP) and (in_port == 10001): 
             ip_pkt = pkt.get_protocols(ipv4.ipv4)[0]
             src = ip_pkt.src
             dst = ip_pkt.dst
 
-            if len(self.port_stats[dpid].keys()) > 0:
+            if len(self.port_stats[dpid].keys()) > 0: 
                 max_pkt = float('-inf')
                 ports = self.port_stats[dpid].keys()
                 ports.sort()
-                max_length = max([self.port_stats[dpid][port_no]['stats']+1 for port_no in ports])
+                max_length = max([self.port_stats[dpid][port_no]['stats'] for port_no in ports])
                 for port_no in ports:
-                    if (np.log(max_length - self.port_stats[dpid][port_no]['stats']) > min_pkt):
-                        max_pkt = np.log(max_length - self.port_stats[dpid][port_no]['stats'])
+                    if port_no < 5000:
+                        P = 1 - self.port_stats[dpid][port_no]['stats']/(1.5 * max_length) 
+                    else:
+                        P = 1 - self.port_stats[dpid][port_no]['stats']/(1.5 * max_length) - 0.2
+                    if (P > max_pkt):
+                        max_pkt = P
                         out_port = port_no
 
                 self.port_stats[dpid][out_port]['users'].add(tuple(sorted([src, dst])))
+                self.port_stats[dpid][out_port]['stats'] += step * (max_length - self.port_stats[dpid][out_port]['stats'])
 
-                actions = [parser.OFPActionSetField(eth_dst="66:66:66:66:66:66"), parser.OFPActionOutput(out_port)]
+                actions = [parser.OFPActionSetField(eth_dst="66:66:66:66:66:66"), parser.OFPActionOutput(out_port)] 
 
-                match_s2d = parser.OFPMatch(in_port=in_port, eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=dst, ipv4_src=src)
+                match_s2d = parser.OFPMatch(in_port=in_port, eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=dst, ipv4_src=src) 
                 match_d2s = parser.OFPMatch(in_port=in_port, eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=src, ipv4_src=dst)
 
 
-                self.add_flow(datapath, 1, match_d2s, actions)
+                self.add_flow(datapath, 1, match_d2s, actions) 
                 if msg.buffer_id != ofproto.OFP_NO_BUFFER:
                     self.add_flow(datapath, 1, match_s2d, actions, msg.buffer_id)
                     return
@@ -341,23 +364,23 @@ class SimpleMonitor13(app_manager.RyuApp):
                 if msg.buffer_id == ofproto.OFP_NO_BUFFER:
                     data = msg.data
 
-                out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                          in_port=in_port, actions=actions, data=data)
+                out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, 
+                                          in_port=in_port, actions=actions, data=data) 
                 datapath.send_msg(out)
 #            else:
 #                return
 #        else:
 #            return
 
-    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER) 
     def _flow_stats_reply_handler(self, ev):
         body = ev.msg.body
         dp_id = ev.msg.datapath.id
 
         time_now = time()
 
-        for stat in sorted([flow for flow in body if flow.priority == 1], key=lambda flow: flow.packet_count):
-            if ('ipv4_src' in stat.match) and ('ipv4_dst' in stat.match):
+        for stat in sorted([flow for flow in body if flow.priority == 1], key=lambda flow: flow.packet_count): 
+            if ('ipv4_src' in stat.match) and ('ipv4_dst' in stat.match): 
                 src = stat.match['ipv4_src']
                 dst = stat.match['ipv4_dst']
                 connection = tuple(sorted([src, dst]))
@@ -376,7 +399,7 @@ class SimpleMonitor13(app_manager.RyuApp):
                     self.flow_stats[dp_id][connection][direction] = stat.packet_count
                     self.flow_stats[dp_id][connection]['time'] = time_now
 
-        for connection in self.flow_stats[dp_id].keys():
+        for connection in self.flow_stats[dp_id].keys(): 
             if self.flow_stats[dp_id].has_key(connection):
                 if time_now > self.flow_stats[dp_id][connection]['time']+float(10):
                     self.port_stats[dp_id][self.flow_stats[dp_id][connection]['port']]['users'].discard(connection)
@@ -392,12 +415,12 @@ class SimpleMonitor13(app_manager.RyuApp):
 
 
 
-    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER) 
     def _port_stats_reply_handler(self, ev):
         body = ev.msg.body
         dp_id = ev.msg.datapath.id
 
-        for stat in sorted(body, key=attrgetter('port_no')):
+        for stat in sorted(body, key=attrgetter('port_no')): 
             port_no = stat.port_no
             if (port_no < 10000) and (port_no not in self.delete_ports[dp_id]):
                 if port_no not in self.port_stats[dp_id]:
@@ -413,13 +436,13 @@ class SimpleMonitor13(app_manager.RyuApp):
 #                    self.logger.info('%016x %8d %8d', dp_id, port_no, self.port_stats[dp_id][port_no]['stats'][-1]-self.port_stats[dp_id][port_no]['stats'][0])
         self.delete_ports[dp_id].clear()
 
-class UFDReceiver(ControllerBase):
+class UFDReceiver(ControllerBase): 
 
     def __init__(self, req, link, data, **config):
         super(UFDReceiver, self).__init__(req, link, data, **config)
         self.ufd_app = data[ufd_app_name]
 
-    @route('ufd', url_overload, methods=['POST'], requirements=None)
+    @route('ufd', url_overload, methods=['POST'], requirements=None) 
     def put_port(self, req, **kwargs):
 
         ufd = self.ufd_app
@@ -432,9 +455,12 @@ class UFDReceiver(ControllerBase):
 
 #        ufd.clean_overload_port(new_entry['port'])
         try:
-            ufd.clean_overload_port(new_entry['port'])
+            user_list = ufd.clean_overload_port(new_entry['port'])
         except Exception as e:
             print(e)
+            
+        if bool(user_list):
+            return Response(status=http_client.OK, body=user_list)
 #            return Response(status=http_client.INTERNAL_SERVER_ERROR)
 
     @route('ufd-statistic', url_port_statistic, methods=['POST'], requirements=None)
